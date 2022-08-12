0c57165192e0d3fcafee4238de5b01cb0ae5b99b
rain <20520@126.com>
2022-08-12 13:33:16 +0800

    finish lru_replacer_test

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,55 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) : capacity_(num_pages) {}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+// Remove the object that was accessed the least recently compared to all the elements being tracked by the Replacer,
+// store its contents in the output parameter and return True. If the Replacer is empty return False.
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+  std::scoped_lock<std::mutex> guard(latch_);
+  if (lru_map_.empty()) {
+    return false;
+  }
+  frame_id_t last = lru_list_.back();
+  lru_map_.erase(last);
+  lru_list_.pop_back();
+  *frame_id = last;
+  return false;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+// This method should be called after a page is pinned to a frame in the BufferPoolManager.
+// It should remove the frame containing the pinned page from the LRUReplacer
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> guard(latch_);
+  if (lru_map_.count(frame_id) != 0) {
+    lru_list_.erase(lru_map_[frame_id]);
+    lru_map_.erase(frame_id);
+  }
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+// This method should be called when the pin_count of a page becomes 0. This method should add the frame containing the
+// unpinned page to the LRUReplacer.
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  std::scoped_lock<std::mutex> guard(latch_);
+  if (lru_map_.count(frame_id) != 0) {
+    return;
+  }
 
-size_t LRUReplacer::Size() { return 0; }
+  while (lru_list_.size() >= capacity_) {
+    frame_id_t last = lru_list_.back();
+    lru_list_.pop_back();
+    lru_map_.erase(last);
+  }
+
+  lru_list_.push_front(frame_id);
+  lru_map_[frame_id] = lru_list_.begin();
+}
+
+size_t LRUReplacer::Size() {
+  std::scoped_lock<std::mutex> guard(latch_);
+  return lru_list_.size();
+}
 
 }  // namespace bustub
